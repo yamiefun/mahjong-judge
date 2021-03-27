043e1225185986a831dc7841f08b2bf90669865f
Henry Yu <henryyu@bigobject.io>
2021-03-27 19:18:49 +0800

    Add card suit, ryuiso checker only cares S.

--- main.py
+++ main.py
@@ -11,18 +11,7 @@ def main():
     while True:
         val = input("Input: ")
         ryuiso.parse_input(val)
-        # if val[0] == 'r':
-        #     sea, hand = defaultdict(int), defaultdict(int)
-        # elif val[0] == 's':
-        #     sea[val[2]] += 1
-        # elif val[0] == 'h':
-        #     hand[val[2]] += 1
-        # else:
-        #     print("Not legal input.")
-        #     continue
-        # print("Sea: ", sea)
-        # print("Hand:", hand)
-        
+
 
 if __name__ == "__main__":
     main()
